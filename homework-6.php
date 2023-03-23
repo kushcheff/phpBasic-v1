049569ae6aa60bae25ffc7d6b1ebe15032f2d459
kushchov.mykyta <kushchov.mykyta@airslate.com>
2023-03-23 21:17:26 +0200

    homework-6

--- homework-6.php
+++ homework-6.php
@@ -3,7 +3,7 @@
 $arr = [];
 
 for ($i = 0; $i <= 29; $i++) {
-        $arr[] = rand(1,50);
+    $arr[] = rand(1, 50);
 }
 print_r($arr);
 
@@ -13,35 +13,52 @@ for ($i = 0; $i < count($arr); $i++) {
     $sum += $arr[$i];
 }
 print_r($sum);
-
-//$fiveCount = 0;
-
-//foreach($arr as $item) {
-//    if($item === 5) {
-//        $fiveCount++;}
-//}
-//echo "\n";
-//print_r($fiveCount);
+echo "-Порахувати суму елементів масиву та вивести на екран.";
 
 $fiveCount = 0;
 $arrCount = count($arr);
 
 for ($i = 0; $i < $arrCount; $i++) {
-    if ($arr[$i] === 5){
+    if ($arr[$i] === 5) {
         $fiveCount++;
     }
+}
+echo "\n";
+print_r($fiveCount);
+echo '-Перевірте скільки раз число 5 зустрічається у вас в масиві, та виведіть це число на екран.';
+
+$numberDivision = 0;
+
+foreach ($arr as $division) {
+    if ($division % 3 === 0) {
+        $numberDivision++;
     }
-    echo "\n";
-    print_r($fiveCount);
+}
+echo "\n";
+print_r($numberDivision);
+echo "-Виведіть на екран тільки числа, які націло діляться на 3.";
 
-//$numberDivision = 0;
-//
-//for ($i = 0; $i < $arrCount; $i++ ) {
-//    if ()
-//}
+$max = $arr[0];
 
+foreach ($arr as $valueMax) {
+    if ($valueMax > $max) {
+        $max = $valueMax;
+    }
+}
+echo "\n";
+print_r($max);
+echo "-max";
+echo "\n" . max($arr) == $max;
 
+$min = $arr[0];
 
-
-
+foreach ($arr as $value) {
+    if ($value < $min) {
+        $min = $value;
+    }
+}
+echo "\n";
+print_r($min);
+echo "-min";
+echo "\n" . min($arr) == $min;
 
