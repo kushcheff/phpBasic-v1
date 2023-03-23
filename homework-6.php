a8b85b456815532dcd3c15284a229c2eb3de4457
kushchov.mykyta <kushchov.mykyta@airslate.com>
2023-03-23 12:24:56 +0200

    homework-6

--- homework-6.php
+++ homework-6.php
@@ -2,11 +2,10 @@
 
 $arr = [];
 
-for ($i = 0; $i <= 30; $i++) {
+for ($i = 0; $i <= 29; $i++) {
         $arr[] = rand(1,50);
 }
 print_r($arr);
-
 
 $sum = 0;
 
@@ -15,15 +14,33 @@ for ($i = 0; $i < count($arr); $i++) {
 }
 print_r($sum);
 
+//$fiveCount = 0;
+
+//foreach($arr as $item) {
+//    if($item === 5) {
+//        $fiveCount++;}
+//}
+//echo "\n";
+//print_r($fiveCount);
 
 $fiveCount = 0;
+$arrCount = count($arr);
 
-foreach($arr as $item) {
-    if($item === 5) {
-        $fiveCount++;}
-}
-echo "\n";
-print_r($fiveCount);
+for ($i = 0; $i < $arrCount; $i++) {
+    if ($arr[$i] === 5){
+        $fiveCount++;
+    }
+    }
+    echo "\n";
+    print_r($fiveCount);
+
+//$numberDivision = 0;
+//
+//for ($i = 0; $i < $arrCount; $i++ ) {
+//    if ()
+//}
+
+
 
 
 
