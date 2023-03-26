40632abdf9af8e98fffc39ad93c834ed0d19e68b
kushchov.mykyta <kushchov.mykyta@airslate.com>
2023-03-26 17:49:23 +0300

    html

---
+++ action_form.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<pre>";
+print_r($_POST);
+print_r($_FILES);
+$uploads_dir = 'uploads';
+foreach ($_FILES as $file) {
+
+$filename = $file['name'];
+$tmp_name = $file['tmp_name'];
+
+move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+}
+echo "<pre>";
+
