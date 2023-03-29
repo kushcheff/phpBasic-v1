5539e9bc1b30aec83e338732bd112ba19e9d3923
kushchov.mykyta <kushchov.mykyta@airslate.com>
2023-03-29 18:51:49 +0300

    add table

--- action_form.php
+++ action_form.php
@@ -4,16 +4,34 @@ echo "<pre>";
 print_r($_POST);
 print_r($_FILES);
 $uploads_dir = 'uploads';
-foreach ($_FILES as $file) {
-    if ($file['error']){
-        echo 'Error';
+
+$files = $_FILES;
+
+if ($files) {
+    foreach ($files as $file) {
+        if (is_array($file['name'])) {
+        $length = count($file['name']);
+        for ($i = 0; $i < $length; $i++) {
+            if ($file['error'][$i]) {
+                echo 'error';
+                continue;
+            }
+
+            $filename = $file['name'][$i];
+            $tmp_name = $file['tmp_name'][$i];
+            move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+        }
+    } else {
+        if ($file['error']) {
+        echo 'error';
         exit;
+        }
+
+            $filename = $file['name'];
+            $tmp_name = $file['tmp_name'];
+            move_uploaded_file($tmp_name, "$uploads_dir/$filename");
+        }
     }
-
-$filename = $file['name'];
-$tmp_name = $file['tmp_name'];
-
-move_uploaded_file($tmp_name, "$uploads_dir/$filename");
 }
 echo "<pre>";
 
